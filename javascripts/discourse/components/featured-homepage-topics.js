c286fb3433a27208f02e5c31a16c2523ae06d14c
awesomerobot <kris.aubuchon@discourse.org>
2022-05-12 22:29:24 -0400

    DEV: update, add body class, simplify settings

--- javascripts/discourse/components/featured-homepage-topics.js
+++ javascripts/discourse/components/featured-homepage-topics.js
@@ -4,59 +4,89 @@ import { inject as service } from "@ember/service";
 import Topic from "discourse/models/topic";
 import { defaultHomepage } from "discourse/lib/utilities";
 
+const FEATURED_CLASS = "featured-homepage-topics";
+
 export default Component.extend({
   classNameBindings: ["featured-homepage-topics"],
   router: service(),
   titleElement: null,
   featuredTagTopics: null,
 
+  _getBanner() {
+    if (!(this.get("isDestroyed") || this.get("isDestroying"))) {
+      let sortOrder = settings.sort_by_created ? "created" : "activity";
+      if (settings.featured_tag) {
+        this.store
+          .findFiltered("topicList", {
+            filter: "latest",
+            params: {
+              tags: [`${settings.featured_tag}`],
+              order: sortOrder,
+            },
+          })
+          .then((topicList) => {
+            let featuredTagTopics = [];
+
+            topicList.topics.forEach((topic) =>
+              topic.image_url ? featuredTagTopics.push(Topic.create(topic)) : ""
+            );
+
+            this.set(
+              "featuredTagTopics",
+              featuredTagTopics.slice(0, settings.number_of_topics)
+            );
+          });
+      }
+    }
+  },
+
+  _checkClass() {
+    if (!this.showHere) {
+      document.querySelector("body").classList.remove(FEATURED_CLASS);
+    }
+  },
+
   init() {
     this._super(...arguments);
-    let sortOrder = settings.sort_by_created ? "created" : "activity";
-    if (settings.featured_tag) {
-      this.store
-        .findFiltered("topicList", {
-          filter: "latest",
-          params: {
-            tags: [`${settings.featured_tag}`],
-            order: sortOrder,
-          },
-        })
-        .then((topicList) => {
-          let featuredTagTopics = [];
+    this._getBanner();
+    this._checkClass();
+  },
 
-          topicList.topics.forEach((topic) =>
-            topic.image_url ? featuredTagTopics.push(Topic.create(topic)) : ""
-          );
+  didInsertElement() {
+    this.appEvents.on("page:changed", this, "_checkClass");
+  },
 
-          this.set(
-            "featuredTagTopics",
-            featuredTagTopics.slice(0, settings.number_of_topics)
-          );
-        });
-    }
+  willDestroyElement() {
+    document.querySelector("body").classList.remove(FEATURED_CLASS);
   },
 
   @discourseComputed("router.currentRoute", "router.currentRouteName")
   showHere(currentRoute, currentRouteName) {
+    let showHere;
+
     if (currentRoute) {
       if (settings.show_on === "homepage") {
-        return currentRouteName == `discovery.${defaultHomepage()}`;
+        showHere = currentRouteName == `discovery.${defaultHomepage()}`;
       } else if (settings.show_on === "top_menu") {
         const topMenuRoutes = this.siteSettings.top_menu
           .split("|")
           .filter(Boolean);
-        return topMenuRoutes.includes(currentRoute.localName);
+        showHere = topMenuRoutes.includes(currentRoute.localName);
       } else if (settings.show_on === "all") {
-        return (
+        showHere =
           currentRouteName.indexOf("editCategory") &&
           currentRouteName.indexOf("admin") &&
-          currentRouteName.indexOf("full-page-search")
-        );
+          currentRouteName.indexOf("full-page-search");
       } else {
         return false;
       }
     }
+
+    if (showHere) {
+      document.querySelector("body").classList.add(FEATURED_CLASS);
+    }
+
+    return showHere;
   },
 
   @discourseComputed()
@@ -70,14 +100,13 @@ export default Component.extend({
 
   @discourseComputed()
   showFor() {
-    if (settings.show_for === "everyone") {
-      return true;
-    } else if (settings.show_for === "logged_out" && !this.currentUser) {
-      return true;
-    } else if (settings.show_for === "logged_in" && this.currentUser) {
+    if (
+      settings.show_for === "everyone" ||
+      (settings.show_for === "logged_out" && !this.currentUser) ||
+      (settings.show_for === "logged_in" && this.currentUser)
+    ) {
       return true;
     } else {
-      document.querySelector("html").classList.remove(FEATURED_CLASS);
       return false;
     }
   },

fd5834139a6ebcb869c722252b2db80602e7beea
Kris <kris.aubuchon@discourse.org>
2022-05-13 11:38:53 -0400

    DEV: add body class, simplify settings, refactor (#8)

--- javascripts/discourse/components/featured-homepage-topics.js
+++ javascripts/discourse/components/featured-homepage-topics.js
@@ -1,8 +1,11 @@
 import discourseComputed from "discourse-common/utils/decorators";
 import Component from "@ember/component";
+import I18n from "I18n";
 import { inject as service } from "@ember/service";
 import Topic from "discourse/models/topic";
 import { defaultHomepage } from "discourse/lib/utilities";
+
+const FEATURED_CLASS = "featured-homepage-topics";
 
 export default Component.extend({
   classNameBindings: ["featured-homepage-topics"],
@@ -10,8 +13,10 @@ export default Component.extend({
   titleElement: null,
   featuredTagTopics: null,
 
-  init() {
-    this._super(...arguments);
+  _getBanner() {
+    if (this.isDestroying || this.isDestroyed) {
+      return;
+    }
     let sortOrder = settings.sort_by_created ? "created" : "activity";
     if (settings.featured_tag) {
       this.store
@@ -37,47 +42,75 @@ export default Component.extend({
     }
   },
 
+  _checkClass() {
+    if (!this.showHere) {
+      document.querySelector("body").classList.remove(FEATURED_CLASS);
+    }
+  },
+
+  init() {
+    this._super(...arguments);
+    this._getBanner();
+    this._checkClass();
+  },
+
+  didInsertElement() {
+    this.appEvents.on("page:changed", this, "_checkClass");
+  },
+
+  willDestroyElement() {
+    this.appEvents.off("page:changed", this, "_checkClass");
+    document.querySelector("body").classList.remove(FEATURED_CLASS);
+  },
+
   @discourseComputed("router.currentRoute", "router.currentRouteName")
   showHere(currentRoute, currentRouteName) {
+    let showHere;
+
     if (currentRoute) {
       if (settings.show_on === "homepage") {
-        return currentRouteName === `discovery.${defaultHomepage()}`;
+        showHere = currentRouteName === `discovery.${defaultHomepage()}`;
       } else if (settings.show_on === "top_menu") {
         const topMenuRoutes = this.siteSettings.top_menu
           .split("|")
           .filter(Boolean);
-        return topMenuRoutes.includes(currentRoute.localName);
+        showHere = topMenuRoutes.includes(currentRoute.localName);
       } else if (settings.show_on === "all") {
-        return (
+        showHere =
           currentRouteName.indexOf("editCategory") &&
           currentRouteName.indexOf("admin") &&
-          currentRouteName.indexOf("full-page-search")
-        );
+          currentRouteName.indexOf("full-page-search");
       } else {
         return false;
       }
     }
+
+    if (showHere) {
+      document.querySelector("body").classList.add(FEATURED_CLASS);
+    }
+
+    return showHere;
   },
 
   @discourseComputed()
   showTitle() {
     if (settings.show_title) {
       const titleElement = document.createElement("h2");
-      titleElement.innerHTML = settings.title_text;
+      titleElement.innerHTML =
+        I18n.t(themePrefix("featured_topic_title")) || settings.title_text;
       return titleElement;
     }
   },
 
   @discourseComputed()
   showFor() {
-    if (settings.show_for === "everyone") {
-      return true;
-    } else if (settings.show_for === "logged_out" && !this.currentUser) {
-      return true;
-    } else if (settings.show_for === "logged_in" && this.currentUser) {
+    if (
+      settings.show_for === "everyone" ||
+      (settings.show_for === "logged_out" && !this.currentUser) ||
+      (settings.show_for === "logged_in" && this.currentUser)
+    ) {
       return true;
     } else {
-      document.querySelector("html").classList.remove(FEATURED_CLASS);
       return false;
     }
   },
---
+++ javascripts/homepage-featured-topics/connectors/discovery-list-controls-above/homepage-featured-topics.js
@@ -0,0 +1,8 @@
+export default {
+  setupComponent(args, component) {
+    component.set(
+      "switchOutlet",
+      settings.above_main_container || settings.show_on === "all"
+    );
+  },
+};
